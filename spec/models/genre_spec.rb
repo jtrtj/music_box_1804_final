1f6978ea2fd1201dc98b80e2927c63ef339a692a
jtrtj <jt.r@live.com>
2018-07-31 10:28:46 -0600

    add avg_rating test to genre test

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -10,10 +10,15 @@ describe Genre, type: :model do
   end
 
   describe 'instance methods' do
+    it '#avg_rating' do
+      artist = Artist.create(name:'Adele')
+      song_1 = artist.songs.create(title: "Don't Stop Believin", length: 303, play_count: 123456, rating: 3)
+      song_2 = artist.songs.create(title: "Never Gonna Give You Up", length: 253, play_count: 98762452, rating: 1)
+      g_1 = Genre.create(name: 'Dance')
+      SongGenre.create(song: song_1, genre: g_1)
+      SongGenre.create(song: song_2, genre: g_1)
     
-  end
-
-  describe 'class methods' do
-    
+      expect(g_1.avg_rating).to eq(2)       
+    end
   end
 end
