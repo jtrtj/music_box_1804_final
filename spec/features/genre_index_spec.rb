1ef8e1a912576f721dff6c545055a09bdf610d2f
jtrtj <jt.r@live.com>
2018-07-31 09:37:44 -0600

    add test for form presence

--- spec/features/genre_index_spec.rb
+++ spec/features/genre_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'a visitor' do
   describe 'visiting genre index' do
-    it 'will see a list of all genres' do
+    it 'will see a list of all genres and their names will be links' do
       g_1 = Genre.create(name: 'Dance')
       g_2 = Genre.create(name: 'Hardcore')
       g_3 = Genre.create(name: 'Emo')
@@ -14,6 +14,22 @@ describe 'a visitor' do
       expect(page).to have_link(g_2.name)
       expect(page).to have_link(g_3.name)
       expect(page).to have_link(g_4.name)
+    end
+  end
+
+  context 'logged in as an admin' do
+    it 'will see a form to create a new genre' do
+      admin = User.create(username: 'admin', password: 'gasd', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      g_1 = Genre.create(name: 'Dance')
+      g_2 = Genre.create(name: 'Hardcore')
+
+      visit genres_path
+
+      expect(page).to have_field("Name")
+      expect(page).to have_button('Create Genre')
+      expect(page).to have_link(g_1.name)
+      expect(page).to have_link(g_2.name)
     end
   end
 end
