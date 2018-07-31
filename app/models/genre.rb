8509c4e2a5af8993c0067e44f1440d742ebfbe92
jtrtj <jt.r@live.com>
2018-07-31 10:29:37 -0600

    add avg_rating to genre model - all tests green

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,8 @@ class Genre < ApplicationRecord
   validates_presence_of :name
   has_many :song_genres
   has_many :songs, through: :song_genres
+
+  def avg_rating
+    songs.average(:rating)
+  end
 end
