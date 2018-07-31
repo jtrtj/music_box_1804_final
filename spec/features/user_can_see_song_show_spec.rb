bb626dd7442cdcdde2446ff5f3cf00c0dd1bc656
jtrtj <jt.r@live.com>
2018-07-31 10:17:58 -0600

    add expectations to see genres for a song on song show

--- spec/features/user_can_see_song_show_spec.rb
+++ spec/features/user_can_see_song_show_spec.rb
@@ -5,6 +5,12 @@ describe 'user sees one song' do
     artist = Artist.create(name: 'Journey')
     song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 5)
     song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908)
+    g_1 = Genre.create(name: 'Dance')
+    g_2 = Genre.create(name: 'Woohoo')
+    g_3 = Genre.create(name: 'Awyeah')
+    SongGenre.create(song: song_1, genre: g_1)
+    SongGenre.create(song: song_1, genre: g_2)
+    SongGenre.create(song: song_2, genre: g_3)
 
     visit song_path(song_1)
 
@@ -13,6 +19,8 @@ describe 'user sees one song' do
     expect(page).to have_content(song_1.title)
     expect(page).to have_content(song_1.length)
     expect(page).to have_content("Rating: #{song_1.rating}")
+    expect(page).to have_content("Genres for this Song: #{g_1.name} #{g_2.name}")
+    expect(page).to_not have_content(g_3.name)
     expect(page).to_not have_content(song_2.title)
     expect(page).to_not have_content(song_2.length)
   end
