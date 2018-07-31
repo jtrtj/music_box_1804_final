98712f046a881e47060b5c3afe19be120143ca73
jtrtj <jt.r@live.com>
2018-07-31 10:46:08 -0600

    add test and view for song with lowest rating

--- spec/features/genre_show_spec.rb
+++ spec/features/genre_show_spec.rb
@@ -54,5 +54,21 @@ describe 'A visitor' do
 
       expect(page).to have_content("Song With Highest Rating: #{g_1.highest_rated_song.title} wiht rating of #{g_1.highest_rated_song.rating}/5")
     end
+
+    it 'will see the name and rating of the song with the lowest rating of all songs in the genre' do
+      artist = Artist.create(name:'Adele')
+      song_1 = artist.songs.create(title: "Don't Stop Believin", length: 303, play_count: 123456, rating: 3)
+      song_2 = artist.songs.create(title: "Never Gonna Give You Up", length: 253, play_count: 98762452, rating: 1)
+      song_3 = artist.songs.create(title: "Hey ya", length: 2543, play_count: 9865452, rating: 5)
+      g_1 = Genre.create(name: 'Dance')
+      g_2 = Genre.create(name: 'Woohoo')
+      SongGenre.create(song: song_1, genre: g_1)
+      SongGenre.create(song: song_2, genre: g_1)
+      SongGenre.create(song: song_3, genre: g_2)
+
+      visit genre_path(g_1)
+
+      expect(page).to have_content("Song With Lowest Rating: #{g_1.lowest_rated_song.title} wiht rating of #{g_1.lowest_rated_song.rating}/5")
+    end
   end
 end
