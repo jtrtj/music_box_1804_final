b166854816d53fe3fb8d2f87a7637cb4d5ea27a4
jtrtj <jt.r@live.com>
2018-07-31 09:18:55 -0600

    add numericality validations to song

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ApplicationRecord
   belongs_to :artist
   has_many :playlist_songs
   has_many :playlists, through: :playlist_songs
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 
   before_save :generate_slug
 
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -5,6 +5,8 @@ describe Song, type: :model do
     it{should validate_presence_of(:title)}
     it{should validate_presence_of(:length)}
     it{should validate_presence_of(:play_count)}
+    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0)}
+    it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
   end
 
   describe 'relationships' do
