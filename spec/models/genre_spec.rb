cda847594699e7f479f215dd0d2cac65b4dbcb00
jtrtj <jt.r@live.com>
2018-07-31 10:42:05 -0600

    add model test for highest_rated_song

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -20,5 +20,16 @@ describe Genre, type: :model do
     
       expect(g_1.avg_rating).to eq(2)       
     end
+
+    it '#highest_rated_song' do
+      artist = Artist.create(name:'Adele')
+      song_1 = artist.songs.create(title: "Don't Stop Believin", length: 303, play_count: 123456, rating: 3)
+      song_2 = artist.songs.create(title: "Never Gonna Give You Up", length: 253, play_count: 98762452, rating: 1)
+      g_1 = Genre.create(name: 'Dance')
+      SongGenre.create(song: song_1, genre: g_1)
+      SongGenre.create(song: song_2, genre: g_1)
+    
+      expect(g_1.highest_rated_song).to eq(song_1)       
+    end
   end
 end
