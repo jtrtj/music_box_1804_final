aaff971e9601d715b33011a59a45409952c96655
jtrtj <jt.r@live.com>
2018-08-01 09:11:44 -0600

    make an admin genres controller for the create action

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -9,19 +9,4 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.includes(:songs).find(params[:id])
   end
-
-  def create
-    @genre = Genre.create(genre_params)
-    if @genre.save
-      redirect_to genres_path
-    else
-      render :index
-    end
-  end
-
-  private
-
-  def genre_params
-    params.require(:genre).permit(:name)
-  end
 end
